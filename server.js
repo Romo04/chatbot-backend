526ea854cba3eace4bb12d57c714ea7ac853563d
rpena <rpena.ort@gmail.com>
2025-10-07 20:57:06 +0400

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const data = require("./lists.json");
+app.use(express.json());
+
+function formatListGeneric(items) {
+  return items
+    .map(item => Object.entries(item)
+      .map(([k, v]) => `${capitalize(k)}: ${v}`)
+      .join("\n"))
+    .join("\n\n");
+}
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+app.post("/get-list", (req, res) => {
+  const category = req.body.category;
+  const items = data[category] || [];
+  res.json({ formatted: formatListGeneric(items) });
+});
+
+app.listen(3000, () => console.log("Running"));
